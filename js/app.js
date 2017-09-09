919c64c42d1c358d1920c6db06935615bf51bb15
ankitamayekar <mayekar.ankita@gmail.com>
2017-09-09 01:47:30 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,104 +1,121 @@
+var map, add1, add2;
+var modelLocations = [
 
-var map,add1,add2;
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'),{
-		center:{lat: 47.639325,lng: -122.128538}, 
-		zoom: 12
+    { title: 'In.gredents', desc: 'Fine Dining Restaurant ', location: { lat: 47.645410, lng: -122.124633 } },
+    { title: 'Spazzo', desc: 'Italian Restaurant Cheery eatery featuring many wines by the glass & Italian basics, with big-screen TV in lively bar.', location: { lat: 47.670385, lng: -122.119998 } },
+    { title: 'Seoul Hot Pot', desc: ' Korean Restaurant Traditional Korean barbecue & other standards offered up in a modest, compact setting.', location: { lat: 47.636620, lng: -122.136993 } },
+    { title: 'Peking', desc: 'Asian Restaurant Family-owned Chinese joint with a focus on housemade noodles plus classic fare since 1983.', location: { lat: 47.628345, lng: -122.151638 } },
+    { title: 'Costco Food Court', desc: 'Located in: Costco Wholesale',location: { lat: 47.681103, lng: -122.181766 } },
+    { title: 'La Casita', desc: 'Festive family restaurant serving a wide variety of Mexican standards with bottomless chips & salsa.', location: { lat: 47.583221, lng: -122.033234 } },
+    { title: 'Taco Time', desc: 'Longtime Mexican fast-food chain serving tacos, crisp burritos & kids meals in simple surrounds.', location: { lat: 47.608226, lng: -122.033234 } },
+    { title: 'Bombay House', desc: 'Indian restaurant specializing in vegetarian & vegan cooking, with gluten-free options available.', location: { lat: 47.576737, lng: -122.136231 } },
+    { title: 'Wild Ginger', desc: 'Busy, sleek restaurant with Pacific Rim fare, happy hour & weekend dim sum brunch.', location: { lat: 47.617993, lng: -122.201366 } }
+];
+var locationView = function (CurrentLocation) {
+    // update the DOM elements with values from the current location
+    var self = this;
+    this.title = CurrentLocation.title;
+    this.lat = CurrentLocation.location.lat;
+    this.lng = CurrentLocation.location.lng;
+    this.add1 = "";
+    this.add2 = "";
+    this.desc = CurrentLocation.desc;
+    this.contentString = "";
+    this.visible = ko.observable(true);
+    //alert(CurrentLocation.desc);
+    /* Importing information from FourSquare 
+    CLIENT_ID
+    0OOSZ1PIEWXX02QKMB42PWJXWZUVIPVCC4L2PFBRYEWKXOMV
+    CLIENT_SECRET
+    DP2GWL0QADHKPXS5TTSZ4S4SRKYU31EM2IOYJN3OZ21ADTOZ
+    version 
+    20161016 */
+    var ResponseURL = 'https://api.foursquare.com/v2/venues/search?ll=' + this.lat + ',' + this.lng + '&client_id=0OOSZ1PIEWXX02QKMB42PWJXWZUVIPVCC4L2PFBRYEWKXOMV&client_secret=DP2GWL0QADHKPXS5TTSZ4S4SRKYU31EM2IOYJN3OZ21ADTOZ&v=20161016&query=' + this.title + '&limit=1';
+    //extract the required information from received information ( address )
+    $.getJSON(ResponseURL).done(function (information) {
+        var resultInfo = information.response.venues[0];
+        if (resultInfo === null) { alert("FourSquare Error."); }
+        self.add1 = resultInfo.location.formattedAddress[0];
+        self.add2 = resultInfo.location.formattedAddress[1];
+        if (self.add1 === "") { self.add1 = "Address could not be found "; }
+        if (self.add2 === "") { self.add2 = "Address could not be found "; }
+    }).fail(function () {
+        alert("FourSquare Error.");
+    });
+	this.marker = new google.maps.Marker({
+		position: new google.maps.LatLng(self.lat, self.lng),
+		map: map,
+		animation: google.maps.Animation.DROP,
+		title: CurrentLocation.title
+	});
+	//marker.addListener('click', toggleBounce);
+	this.marker.addListener('click', function () {
+		//add info window to the marker
+		self.contentString = '<div><div><b>' + CurrentLocation.title + '</b></div><div>' + self.add1 + '</div><div>' + self.add2 + '</div><hr><div>' + CurrentLocation.desc + '</div></div>';
+		self.infowindow = new google.maps.InfoWindow({
+			content: self.contentString,
+			maxWidth: 200
 		});
-}
-var modelLocations =[
-		
-        {title: 'In.gredents',desc:'Fine Dining Restaurant ',area:'redmond',location: {lat: 47.645410, lng: -122.124633}},
-        {title: 'Spazzo',desc:'Italian Restaurant Cheery eatery featuring many wines by the glass & Italian basics, with big-screen TV in lively bar.', area:'redmond',location: {lat: 47.670385, lng: -122.119998}}, 
-        {title: 'Seoul Hot Pot',desc:' Korean Restaurant Traditional Korean barbecue & other standards offered up in a modest, compact setting.',area:'redmond',location: {lat: 47.636620, lng: -122.136993}}, 
-        {title: 'Peking', desc:'Asian Restaurant Family-owned Chinese joint with a focus on housemade noodles plus classic fare since 1983.',area:'redmond', location: {lat: 47.628345, lng: -122.151638}},
-	    {title: 'Costco Food Court', desc:'Located in: Costco Wholesale',area:'Kirkland', location: {lat: 47.681103, lng: -122.181766}},
-		{title: 'La Casita', desc:'Festive family restaurant serving a wide variety of Mexican standards with bottomless chips & salsa.',area:'sammamish', location: {lat: 47.583221, lng: -122.033234}},
-		{title: 'Taco Time', desc:'Longtime Mexican fast-food chain serving tacos, crisp burritos & kids meals in simple surrounds.',area:'Issaquah', location: {lat: 47.608226, lng: -122.033234}},
-		{title: 'Bombay House', desc:'Indian restaurant specializing in vegetarian & vegan cooking, with gluten-free options available.',area:'Bellevue', location: {lat: 47.576737, lng: -122.136231}},
-        {title: 'Wild Ginger',desc:'Busy, sleek restaurant with Pacific Rim fare, happy hour & weekend dim sum brunch.',area:'bellevue',location: {lat: 47.617993, lng: -122.201366}}
-] ;
-var locationView =  function(CurrentLocation) {
-        // update the DOM elements with values from the current location
-        var self = this;
-		this.title = CurrentLocation.title;
-		this.lat = CurrentLocation.location.lat;
-		this.lng = CurrentLocation.location.lng;
-		this.add1 = "";
-		this.add2 = "";
-		this.desc = CurrentLocation.desc;
-		this.contentString = "";
-		this.visible = ko.observable(true);
-		//alert(CurrentLocation.desc);
-		/* Importing information from FourSquare 
-		CLIENT_ID
-		0OOSZ1PIEWXX02QKMB42PWJXWZUVIPVCC4L2PFBRYEWKXOMV
-		CLIENT_SECRET
-		DP2GWL0QADHKPXS5TTSZ4S4SRKYU31EM2IOYJN3OZ21ADTOZ
-		version 
-		20161016 */
-		var ResponseURL ='https://api.foursquare.com/v2/venues/search?ll='+this.lat+','+this.lng+'&client_id=0OOSZ1PIEWXX02QKMB42PWJXWZUVIPVCC4L2PFBRYEWKXOMV&client_secret=DP2GWL0QADHKPXS5TTSZ4S4SRKYU31EM2IOYJN3OZ21ADTOZ&v=20161016&query='+this.title+'&limit=1';
-		//extract the required information from received information ( address )
-		$.getJSON(ResponseURL).done(function(information) {
-			var resultInfo = information.response.venues[0];
-			self.add1 = resultInfo.location.formattedAddress[0];
-			self.add2 = resultInfo.location.formattedAddress[1];
-		 }).fail(function() {
-			alert("FourSquare Error.");
-		});
-		this.openinfowindow = function(place) {
-			this.marker = new google.maps.Marker({
-				position: new google.maps.LatLng(self.lat , self.lng),
-				map:map,
-				animation: google.maps.Animation.DROP,
-				title:CurrentLocation.title	
-			});
-			
-			
-			//marker.addListener('click', toggleBounce);
-			this.marker.addListener('click', function() {
-				//add info window to the marker
-				self.contentString ='<div><div><b>'+CurrentLocation.title+'</b></div><div>'+self.add1 +'</div><div>'+self.add2 +'</div><hr><div>'+CurrentLocation.desc +'</div></div>';
-				self.infowindow = new google.maps.InfoWindow({
-					content: self.contentString,
-					maxWidth: 200
-				}); 
-				self.infowindow.open(map, this);
-				self.marker.setAnimation(google.maps.Animation.BOUNCE);
-				setTimeout(function() {
-					self.marker.setAnimation(null);
-				}, 2000); 
-				
-			});
-		};
+		self.infowindow.open(map, this);
+		self.marker.setAnimation(google.maps.Animation.BOUNCE);
+		setTimeout(function () {self.marker.setAnimation(null);}, 2000);
+		setTimeout(function () {self.infowindow.close();}, 5000);
+
+	});
+	//trigger the marker from the list 
+	this.openinfowindow = function (place) {
+		new google.maps.event.trigger(self.marker, 'click');
+	};
+	//sorting the markers according to the search 
+	this.sortMarker = ko.computed(function() {
+		if(this.visible() === true) {
+			this.marker.setMap(map);
+		} else {
+			this.marker.setMap(null);
+			//this.marker.setVisible(false);
+		}
+		return true;
+	}, this);
 };
 
-function AppViewModel(){
-	var self = this;
-	this.myInput = ko.observable("");
-	this.locArray = ko.observableArray([]);
-	
-	modelLocations.forEach(function(restinfo){
-		self.locArray.push( new locationView(restinfo));
-	});
-	this.displayArray = ko.computed( function() {
-		var input = self.myInput().toUpperCase();
+function AppViewModel() {
+	map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 47.639325, lng: -122.128538 },
+        zoom: 12
+    });
+    var self = this;
+    this.myInput = ko.observable("");
+    this.locArray = ko.observableArray([]);
 
-		if (input==="") {
-			self.locArray().forEach(function(restinfo){
-				restinfo.visible(true);
-			});
-			return self.locArray();
-			
-		} else {
-			return ko.utils.arrayFilter(self.locArray(), function(restinfo) {
-			var string = restinfo.title.toUpperCase();
-			var result = (string.includes(input) === true);
-				restinfo.visible(result);
-				return result;
-			});
-		}
-	}, self);
+    modelLocations.forEach(function (restinfo) {
+        self.locArray.push(new locationView(restinfo));
+    });
+    this.displayArray = ko.computed(function () {
+        var input = self.myInput().toUpperCase();
+
+        if (input === "") {
+            self.locArray().forEach(function (restinfo) {
+                restinfo.visible(true);
+            });
+            return self.locArray();
+
+        } else {
+            return ko.utils.arrayFilter(self.locArray(), function (restinfo) {
+                var string = restinfo.title.toUpperCase();
+                var result = (string.includes(input) === true);
+                restinfo.visible(result);
+                return result;
+            });
+        }
+    }, self);
 }
 // Activates knockout.js
+function initMap() {
 	ko.applyBindings(new AppViewModel());
+}
+
+
+// error handling
+function myErrorFunction() {
+    alert('Unable to Load Google Maps.');
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+Udacity Project Neighborhood Map.
+
+Its a single page application featuring a map of My neighborhood.There is a functionality to this map including highlighted locations, third-party data about those locations and various ways to browse the content.
+The third party use to take data from is FourSquare I am using Address of the location from foursquare to be shown in infoWindow.
+the various methods to browse the content are through the list displayed on the right side of the map or the search box above the list.
+the site is responsive it changes the display if the screen is less than 500px.
+
+github Link :https://github.com/ankitamayekar/neighborhood-map 
+
+click the link and download or creat a github repositoy.
+Create a google APIKEY and replace it with (AIzaSyB50yccHkgm_Qz6YwsZkMnZB9y08YYHBXY) in index.html file line 29.
+Get Google Api Key here: https://developers.google.com/maps/documentation/geocoding/start#get-a-key.
+Create foursquare client Id, Client secrete and version and replace the same in js/app.js file line 33.
+Get Foursquare keys here: https://developer.foursquare.com/
+open index.html in a browser.
+click on any resturant name to see the location and the information window.
+search for a specific resturant in the search bar.
+  
